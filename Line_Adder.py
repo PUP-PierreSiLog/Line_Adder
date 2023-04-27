44e7fc8160a14d1dd6bd22691e8893b2bd2960d1
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-27 22:18:05 +0800

    Fixed the error of continuing by adding an if statement inside the while loop. Also done is the creation of write commands to the code.

--- Line_Adder.py
+++ Line_Adder.py
@@ -2,12 +2,17 @@ import sys
 #Creates a file to write
 with open("D:\[CMPE103] OOP\Line_Adder\\mylife.txt", "w") as source:
     #Create a method to write multiple line of text contents
-    line=input("Enter line:")
-    choice = input("Are there more lines? y/n")
+    init=input("Welcome to the program! Are you ready to start? y/n")
 
     #While loop for the variables
-    while "y" in choice:
+    while "y" in init.lower():
         line=input("Enter line:")
+        source.write(line+"\n")
         choice = input("Are there more lines? y/n")
+        if "y" in choice.lower():
+            source.write(choice+"\n")
+            continue
+        else:
+            sys.exit()
     else:
         sys.exit()

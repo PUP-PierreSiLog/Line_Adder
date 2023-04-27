016931f9ce4cb4bc8008492aa399ce59576bb055
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-27 21:56:51 +0800

    Created a basic def for variables

--- Line_Adder.py
+++ Line_Adder.py
@@ -1,4 +1,12 @@
+import sys
 #Creates a file to append
 #Create a method to write multiple line of text contents
-line=input("Enter line:")
-choice = input("Are there more lines? y/n")
+def basic(line, choice):
+    line=input("Enter line:")
+    choice = input("Are there more lines? y/n")
+
+#If loop for the variables
+if "y" in choice:
+    basic()
+else:
+    sys.exit()

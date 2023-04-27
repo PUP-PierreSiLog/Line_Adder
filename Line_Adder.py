76087e6fafbb2bfd4ac07ef3a5fb3f6a6c472823
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-27 22:06:16 +0800

    Removed function, change to while loop

--- Line_Adder.py
+++ Line_Adder.py
@@ -1,12 +1,12 @@
 import sys
 #Creates a file to append
 #Create a method to write multiple line of text contents
-def basic(line, choice):
+line=input("Enter line:")
+choice = input("Are there more lines? y/n")
+
+#While loop for the variables
+while "y" in choice:
     line=input("Enter line:")
     choice = input("Are there more lines? y/n")
-
-#If loop for the variables
-if "y" in choice:
-    basic()
 else:
     sys.exit()

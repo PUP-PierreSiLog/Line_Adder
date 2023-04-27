49b7c221674033be67530c3b75cf11bce1fd3ca9
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-04-27 22:09:49 +0800

    Created a code that commands writing a file called mylife.txt

--- Line_Adder.py
+++ Line_Adder.py
@@ -1,12 +1,13 @@
 import sys
-#Creates a file to append
-#Create a method to write multiple line of text contents
-line=input("Enter line:")
-choice = input("Are there more lines? y/n")
-
-#While loop for the variables
-while "y" in choice:
+#Creates a file to write
+with open("D:\[CMPE103] OOP\Line_Adder\\mylife.txt", "w") as source:
+    #Create a method to write multiple line of text contents
     line=input("Enter line:")
     choice = input("Are there more lines? y/n")
-else:
-    sys.exit()
+
+    #While loop for the variables
+    while "y" in choice:
+        line=input("Enter line:")
+        choice = input("Are there more lines? y/n")
+    else:
+        sys.exit()
